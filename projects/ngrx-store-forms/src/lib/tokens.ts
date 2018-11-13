0040e6c5a759f2416716548c7c193406224301db
Gion Kunz <gion@syncrea.ch>
2018-11-13 07:56:26 -0800

    Fixed wrong token typing

--- projects/ngrx-store-forms/src/lib/tokens.ts
+++ projects/ngrx-store-forms/src/lib/tokens.ts
@@ -1,5 +1,5 @@
 import {InjectionToken} from '@angular/core';
 import {StoreFormsConfig} from './store-forms.model';
 
-export const STORE_FORMS_CONFIG = new InjectionToken<() => StoreFormsConfig>('rxsf Config');
+export const STORE_FORMS_CONFIG = new InjectionToken<StoreFormsConfig>('rxsf Config');
 export const STORE_FORMS_FEATURE = new InjectionToken<string>('rxsf Feature');
